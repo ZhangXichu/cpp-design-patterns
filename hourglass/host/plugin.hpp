95180ce3d917ff8739233eeb7c9b5627629d7df2
ZhangXichu <xichu.zhang@yahoo.com>
2026-08-15 19:09:09 +0200

    add include guard; direct include

--- hourglass/host/plugin.hpp
+++ hourglass/host/plugin.hpp
@@ -1,25 +1,27 @@
+#pragma once
+
 /**
  * @file plugin.hpp
  * @brief The C++ API exposed to the end user
- * 
+ *
  */
 
- #include <filesystem>
+#include <cstdint>
+#include <filesystem>
+#include <memory>
 #include <plugin_api.h>
+#include <string>
 #include <variant>
-
 
 using PluginResult = std::variant<int32_t, std::string>;
 
 class Plugin {
-public:
+  public:
     explicit Plugin(std::filesystem::path path);
     ~Plugin();
     PluginResult execute(const char *input) const;
 
-private:
+  private:
     struct Impl;
     std::unique_ptr<Impl> _impl;
-
 };
-
